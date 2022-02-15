f41523ca394163a47a8006c6779849d6d99a5c95
shidai287 <shidai287@gotvdsb.ca>
2022-02-14 19:14:55 -0500

    adding command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.SpinSpark;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Climber;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -29,7 +31,7 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
 
-    m
+    m_climber.setDefaultCommand(SpinSpark(m_climber, 0.5));
   }
 
   /**
