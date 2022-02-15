4ae41d980527048fafb33366993397db806e0f15
shidai287 <shidai287@gotvdsb.ca>
2022-02-14 19:09:05 -0500

    Copied

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants;
 
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -12,35 +11,17 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class DriveTrain extends SubsystemBase {
 
-    private final WPI_TalonFX leftSecondaryMotor;
-    private final WPI_TalonFX leftPrimaryMotor;
-    private final WPI_TalonFX rightSecondaryMotor;
-    private final WPI_TalonFX rightPrimaryMotor;
+    private final WPI_TalonFX leftSecondaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_LEFT_SECONDARY_MOTOR);;
+    private final WPI_TalonFX leftPrimaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_LEFT_PRIMARY_MOTOR);;
+    private final WPI_TalonFX rightSecondaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_RIGHT_SECONDARY_MOTOR);;
+    private final WPI_TalonFX rightPrimaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_RIGHT_PRIMARY_MOTOR);;
 
 
     
-    private final MotorControllerGroup m_leftMotors;
-    private final MotorControllerGroup m_rightMotors;
+    private final MotorControllerGroup m_leftMotors = new MotorControllerGroup(leftPrimaryMotor, leftSecondaryMotor);;
+    private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(rightPrimaryMotor, rightSecondaryMotor);;
 
-    private final DifferentialDrive m_drive;
-
-    public DriveTrain() {
-        // super();
-
-        leftSecondaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_LEFT_SECONDARY_MOTOR);
-        leftPrimaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_LEFT_PRIMARY_MOTOR);
-        rightSecondaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_RIGHT_SECONDARY_MOTOR);
-        rightPrimaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_RIGHT_PRIMARY_MOTOR);
-
-
-        m_leftMotors = new MotorControllerGroup(leftPrimaryMotor, leftSecondaryMotor);
-        m_rightMotors = new MotorControllerGroup(rightPrimaryMotor, rightSecondaryMotor);
-
-        m_leftMotors.setInverted(true);
-        m_rightMotors.setInverted(true);
-
-        m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
-    }
+    private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);;
 
     public void arcadeDrive(double fwd, double rot) {
         m_drive.arcadeDrive(fwd, rot);
