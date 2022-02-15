a4de22b82079430c9eee15de24ca532510008ed2
shidai287 <shidai287@gotvdsb.ca>
2022-02-15 17:56:22 -0500

    let me look at example

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,6 +2,23 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.Constants;
+
+import com.revrobotics.CANSparkMax;
+
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class Shooter extends SubsystemBase{
-    
+    private final CANSparkMax topConveryorMotor = new CANSparkMax(Constants.ShooterConstant.CAN_ADDRESS_TOP_CONVEYOR_MOTOR, )
+
+
+    public double method1() {
+
+    }
+
+    public void methosd2() {
+        
+    }
+
+
 }
