018a2406f65a907b47d0a8259a10e1446ee5d048
shidai287 <shidai287@gotvdsb.ca>
2022-02-27 09:21:02 -0500

    smartdash

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants;
@@ -11,9 +13,14 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class Intake extends SubsystemBase {
     
     private final CANSparkMax intakeMotor = new CANSparkMax(Constants.IntakeConstants.CAN_ADDRESS_INTAKE_MOTOR, MotorType.kBrushless);
+    private final Solenoid intakeSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 2);
 
     public void rollIntake(double speed) {
         intakeMotor.set(speed);
+    }
+
+    public void runSolenoid() {
+        intakeSolenoid.toggle();
     }
     
 }
