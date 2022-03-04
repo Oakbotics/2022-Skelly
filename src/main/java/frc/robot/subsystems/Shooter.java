93dddef3b3978db9f0bce4217d85d225eb97fc97
Ali-F-Q <qureali987@gotvdsb.ca>
2022-03-04 13:08:09 -0500

    Attempt at making a method

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -10,6 +10,9 @@ import com.revrobotics.CANSparkMax;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+//import edu.wpi.first.wpilibj.Timer;
+//import com.revrobotics.CANSparkMax.ControlType.kVelocity
+
 
 public class Shooter extends SubsystemBase{
     private final CANSparkMax topConveryorMotor = new CANSparkMax(Constants.ShooterConstant.CAN_ADDRESS_TOP_CONVEYOR_MOTOR, MotorType.kBrushless);
@@ -17,6 +20,9 @@ public class Shooter extends SubsystemBase{
     private final CANSparkMax leftShooterMotor = new CANSparkMax(Constants.ShooterConstant.CAN_ADDRESS_LEFT_SHOOTER_MOTOR, MotorType.kBrushless);
     private final CANSparkMax rightShooterMotor = new CANSparkMax(Constants.ShooterConstant.CAN_ADDRESS_RIGHT_SHOOTER_MOTOR, MotorType.kBrushless);
     private final VictorSPX kicker = new VictorSPX(Constants.ShooterConstant.CAN_ADDRESS_KICKER);
+
+    //double final maxRPM = 5700;
+    //double velocity;
 
     public void runConveryor(double speed) {
         topConveryorMotor.set(speed);
@@ -31,5 +37,8 @@ public class Shooter extends SubsystemBase{
     public void runKicker() {
         kicker.set(ControlMode.PercentOutput, 1);
     }
-
+    
+    //public void shooting()  {
+    //    velocity.getVelocity();
+    //}
 }
