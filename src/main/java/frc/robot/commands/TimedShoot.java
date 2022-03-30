fc349c59cb1f5e3a0f7084fc5c47b5f60e4d3867
shidai287 <shidai287@gotvdsb.ca>
2022-03-30 15:01:46 -0400

    simple timed auto complete

---
+++ src/main/java/frc/robot/commands/TimedShoot.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.Shooter;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class TimedShoot extends CommandBase{
+    private final Shooter m_shooter;
+    double startTime;
+    double time;
+
+    public TimedShoot(Shooter m_shooter) {
+        this.m_shooter = m_shooter;
+        addRequirements(this.m_shooter);
+    }
+
+    @Override
+    public void initialize() {
+        startTime  = Timer.getFPGATimestamp();
+        time = Timer.getFPGATimestamp();
+    }
+
+    @Override
+    public void execute() {
+        m_shooter.runShooter(0.20);
+        m_shooter.runKicker(0.75);
+        m_shooter.runConveryor(0.90);
+        time = Timer.getFPGATimestamp();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_shooter.runShooter(0);
+        m_shooter.runKicker(0);
+        m_shooter.runConveryor(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return time - startTime > 2;
+    }
+}
