c4e6a3de1d98111783194510ec3d636dd18431e0
shidai287 <shidai287@gotvdsb.ca>
2022-04-01 16:09:43 -0400

    tmr will be fun

--- src/main/java/frc/robot/commandGroups/TimedAuto.java
+++ src/main/java/frc/robot/commandGroups/TimedAuto.java
@@ -2,7 +2,7 @@ package frc.robot.commandGroups;
 
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
+import frc.robot.commands.RunConveryor;
 import frc.robot.commands.TimedDrive;
 import frc.robot.commands.TimedShoot;
 import frc.robot.subsystems.DriveTrain;
@@ -12,6 +12,8 @@ public class TimedAuto extends SequentialCommandGroup{
     
     public TimedAuto(DriveTrain m_driveTrain, Shooter m_shooter){
         addCommands(
+            new RunConveryor(m_shooter, -0.90),
+            
             new TimedShoot(m_shooter),
 
             new TimedDrive(m_driveTrain)
